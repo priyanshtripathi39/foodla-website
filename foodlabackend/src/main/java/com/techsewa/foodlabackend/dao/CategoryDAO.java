f627bfefa6751493ff77de2905146eabffd41ee6
priyanshtripathi39 <priyanshtripathi39@gmail.com>
2018-01-15 10:41:00 +0530

    hibernate configration done...

--- foodlabackend/src/main/java/com/techsewa/foodlabackend/dao/CategoryDAO.java
+++ foodlabackend/src/main/java/com/techsewa/foodlabackend/dao/CategoryDAO.java
@@ -6,9 +6,14 @@ import com.techsewa.foodlabackend.dto.Category;
 
 public interface CategoryDAO {
 	
-	List<Category> list();
+	
+	
 	
 	Category get(int id);
 	
+	List<Category> list();
 
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
 }
--- foodlabackend/src/main/java/com/techsewa/foodlabackend/dto/Category.java
+++ foodlabackend/src/main/java/com/techsewa/foodlabackend/dto/Category.java
@@ -1,16 +1,35 @@
 package com.techsewa.foodlabackend.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 	
 	//Private Fields
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
-	private String description;
+	
+	private String description;		
+	
+	@Column(name="image_url")
 	private String imageURL;
+	
+	@Column(name="is_active")
 	private boolean active=true;
 	
 	
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + ", description=" + description + ", imageURL=" + imageURL
+				+ ", active=" + active + "]";
+	}
 	public int getId() {
 		return id;
 	}
